3919762af7971e64e7cc034950fe2b2d3a2392c6
Hannes Mehnert <hannes@mehnert.org>
2025-03-01 00:22:09 +0100

    add phase2 code (from archive_opam)

--- maintenance_intent.ml
+++ maintenance_intent.ml
@@ -124,28 +124,31 @@ let not_maintained (_, opam) =
   | Some { pelem = Bool b ; _ } -> b
   | _ -> invalid_arg "maintained: expected a bool"
 
-let jump () opam_repository pkgs pkg_all =
-  let ( let* ) = Result.bind in
-  let pkg_dir = Fpath.(v opam_repository / "packages") in
-  let* _ = Bos.OS.Dir.must_exist pkg_dir in
-  let contexts = List.map (context opam_repository) ocaml_versions in
-  let pkgs = match pkgs, pkg_all with
-    | [], true ->
-      let dh = Unix.opendir (Fpath.to_string pkg_dir) in
-      let rec loop acc =
-        match Unix.readdir dh with
-        | exception End_of_file -> acc
-        | pkg -> loop (pkg :: acc)
-      in
-      let acc = loop [] in
-      Unix.closedir dh;
-      acc
-    | _, true -> invalid_arg "both --pkg and --pkg-all not supported"
-    | _, false -> pkgs
-  in
-  let should_consider path =
-    if Fpath.filename path = "opam" then
-      let _name, version = pkg_name_and_version path in
+let eval_pkgs pkg_dir pkgs pkg_all =
+  match pkgs, pkg_all with
+  | [], true ->
+    let dh = Unix.opendir (Fpath.to_string pkg_dir) in
+    let rec loop acc =
+      match Unix.readdir dh with
+      | exception End_of_file -> acc
+      | pkg -> loop (pkg :: acc)
+    in
+    let acc = loop [] in
+    Unix.closedir dh;
+    acc
+  | _, true -> invalid_arg "both --pkg and --pkg-all not supported"
+  | _, false -> pkgs
+
+let should_consider ?excluded path =
+  if Fpath.filename path = "opam" then
+    let _name, version = pkg_name_and_version path in
+    let is_excluded = match excluded with
+      | None -> false
+      | Some x -> List.mem version x
+    in
+    if is_excluded then
+      None
+    else
       let opam =
         let opam_file =
           OpamFile.make (OpamFilename.raw (Fpath.to_string path))
@@ -153,84 +156,352 @@ let jump () opam_repository pkgs pkg_all =
         OpamFile.OPAM.read opam_file
       in
       Some (version, opam)
-    else
-      None
-  in
+  else
+    None
+
+let find_opams_latest_first pkg_dir pkg =
+  let ( let* ) = Result.bind in
   let foreach path acc =
     let* opams = acc in
     match should_consider path with
     | None -> Ok opams
     | Some p -> Ok (p :: opams)
   in
-  List.fold_left (fun acc pkg ->
-      (* for each pkg, we collect all opam files *)
-      let* () = acc in
-      let* opams =
-        Bos.OS.Dir.fold_contents foreach (Ok []) Fpath.(pkg_dir / pkg)
+  let* opams =
+    Bos.OS.Dir.fold_contents foreach (Ok []) Fpath.(pkg_dir / pkg)
+  in
+  let* opams = opams in
+  Ok (List.sort (fun (v, _) (v', _) ->
+      OpamVersionCompare.compare (version v') (version v))
+      opams)
+
+let decode_intent pkg opam =
+  let intent =
+    let default = "(any)" in
+    let open OpamParserTypes.FullPos in
+    match OpamFile.OPAM.extended opam "x-maintenance-intent" Fun.id with
+    | None -> default
+    | Some { pelem = List { pelem = [ one ] ; _ } ; _ } ->
+      let extract_string = function
+        | { pelem = String s ; _ } -> s
+        | x ->
+          Logs.warn (fun m -> m "%s intent failure: expected a string, got %s" pkg
+                        (OpamPrinter.FullPos.value x));
+          default
       in
-      let* opams = opams in
-      let sorted =
-        List.sort
-          (fun (v, _) (v', _) -> OpamVersionCompare.compare (version v') (version v))
-          opams
+      extract_string one
+    | Some x ->
+      Logs.warn (fun m -> m "%s intent failure: expected a list of a single string, got %s"
+                    pkg (OpamPrinter.FullPos.value x));
+      default
+  in
+  try Mintent.M.intent_of_string intent with
+  | Failure _ ->
+    Logs.warn (fun m -> m "%s invalid intent: %s, using any" pkg intent);
+    [ Mintent.M.Last max_int ]
+
+let eval_intent contexts pkg sorted (intent : Mintent.M.intent) =
+  match intent with
+  | Mintent.M.Last x :: [] when x = max_int ->
+    (* if any, we output all *)
+    let opams = List.filter not_maintained sorted in
+    let keeping = List.map fst opams in
+    let remove =
+      let s = S.of_list (List.map fst sorted) in
+      S.elements (S.diff s (S.of_list keeping))
+    in
+    (* Logs.app (fun m -> m "%s intent is any" pkg); *)
+    List.iter (fun pkg -> Logs.app (fun m -> m "REMOVING (any) %s" pkg)) remove;
+    remove
+  | Mintent.M.Last 1 :: [] ->
+    (* latest! we go through all ocaml versions and find the latest package *)
+    let keeping = solve_by_ocaml_version contexts pkg in
+    (* let opams = find_latest sorted in
+       let opams = List.filter not_maintained (M.bindings opams) in
+       let keeping = List.map fst opams in *)
+    let remove =
+      S.elements (S.diff (S.of_list (List.map fst sorted)) keeping)
+    in
+    (*Logs.app (fun m -> m "%s intent is latest" pkg);*)
+    List.iter (fun pkg -> Logs.app (fun m -> m "REMOVING (latest) %s" pkg))
+      remove;
+    remove
+  | Mintent.M.Last 0 :: [] ->
+    (*Logs.app (fun m -> m "%s intent is none, keeping nothing" pkg);*)
+    let remove = List.map fst sorted in
+    List.iter (fun pkg -> Logs.app (fun m -> m "REMOVING (none) %s" pkg))
+      remove;
+    remove
+  | _ ->
+    let opams = List.filter not_maintained sorted in
+    let keeping = List.map fst opams in
+    let remove =
+      S.elements (S.diff (S.of_list (List.map fst sorted)) (S.of_list keeping))
+    in
+    let int = Mintent.M.string_of_intent intent in
+    Logs.warn (fun m -> m "%s intent is %s (not handled)" pkg int);
+    List.iter (fun pkg -> Logs.app (fun m -> m "REMOVING (%s) %s" int pkg))
+      remove;
+    remove
+
+let string_of_relop = OpamPrinter.FullPos.relop_kind
+
+let rec filter_to_string = function
+  | OpamTypes.FBool b -> string_of_bool b
+  | FString s -> "\"" ^ s ^ "\""
+  | FIdent (_opt_names, var, _env) ->
+    OpamVariable.to_string var
+  | FOp (f, rel, g) ->
+    filter_to_string f ^ " " ^ string_of_relop rel ^ " " ^ filter_to_string g
+  | FAnd (f, g) ->
+    filter_to_string f ^ " && " ^ filter_to_string g
+  | FOr (f, g) ->
+    filter_to_string f ^ " || " ^ filter_to_string g
+  | FNot f -> "not " ^ filter_to_string f
+  | FDefined f -> "defined " ^ filter_to_string f
+  | FUndef f -> "undefined " ^ filter_to_string f
+
+let f_to_string = function
+  | OpamTypes.Filter filter -> filter_to_string filter
+  | Constraint (relop, filter) ->
+    string_of_relop relop ^ " " ^ filter_to_string filter
+
+let rec condition_to_string = function
+  | OpamTypes.Empty -> ""
+  | Atom f -> f_to_string f
+  | Block a -> condition_to_string a
+  | And (a, b) -> condition_to_string a ^ " & " ^ condition_to_string b
+  | Or (a, b) -> condition_to_string a ^ " | " ^ condition_to_string b
+
+let is_installable opams opam =
+  Logs.info (fun m -> m "installable of opam %s version %s"
+               (OpamPackage.Name.to_string (OpamFile.OPAM.name opam))
+               (OpamPackage.Version.to_string (OpamFile.OPAM.version opam)));
+  let find_available name =
+    OpamPackage.Set.fold (fun pkg acc ->
+        if OpamPackage.Name.equal pkg.OpamPackage.name name then
+          (OpamPackage.Version.to_string pkg.OpamPackage.version) :: acc
+        else
+          acc) opams []
+  in
+  let current_deps = OpamFile.OPAM.depends opam in
+  let rec deps_good e = function
+    | OpamFormula.Empty -> true, e
+    | Atom (name, condition) ->
+      Logs.info (fun m -> m "deps good %s" (OpamPackage.Name.to_string name));
+      (* we've to find name in opams, and figure whether condition is satisfied *)
+      let all_available = find_available name in
+      (* esp. in conjunctions ("foo" {>= "1.0" & < "1.3"}) we need to track the
+         matching candidates across the "&" - otherwise we may select "2.0" for
+         the first conjunct, and 0.3 for the second. That's why we use a
+         reference cell here. *)
+      let available = ref all_available in
+      let relop_cmp x = function
+        | `Eq -> x = 0
+        | `Geq -> x >= 0
+        | `Gt -> x > 0
+        | `Leq -> x <= 0
+        | `Lt -> x < 0
+        | `Neq -> x <> 0
       in
-      (* we parse the x-maintenance-intent of the last version *)
-      let* intent =
-        let default = "(any)" in
-        let open OpamParserTypes.FullPos in
-        match OpamFile.OPAM.extended (snd (List.hd sorted)) "x-maintenance-intent" Fun.id with
-        | None -> Ok default
-        | Some { pelem = List { pelem = [ one ] ; _ } ; _ } ->
-          let extract_string = function
-            | { pelem = String s ; _ } -> Ok s
-            | x ->
-              Logs.warn (fun m -> m "%s intent failure: expected a string, got %s" pkg
-                            (OpamPrinter.FullPos.value x));
-              Ok default
+      let combine_f op a b = match a, b with
+        | `Bool a, `Bool b -> `Bool (op a b)
+        | `Always, _ | _, `Always -> `Always
+      in
+      let rec matches_filter = function
+        | OpamTypes.FBool b -> `Bool b
+        | FString s ->
+          Logs.info (fun m -> m "matches_filter: string %s" s);
+          `Bool true
+        | FIdent (_opt_names, var, _env) ->
+          begin match OpamVariable.to_string var with
+            | "dev" | "with-doc" | "with-test" | "with-dev-setup" -> `Always
+            | "build" | "post" -> `Bool true
+            | _ ->
+              Logs.info (fun m -> m "matches_filter: ident %s" (OpamVariable.to_string var));
+              `Bool true
+          end
+        | FOp (f, rel, g) ->
+          begin match f with
+            | FIdent (_, var, _) when OpamVariable.to_string var = "os" ->
+              `Bool true
+            | _ ->
+              Logs.info (fun m -> m "matches_filter: %s %s %s" (filter_to_string f)
+                            (string_of_relop rel) (filter_to_string g));
+              `Bool true
+          end
+        | FAnd (f, g) ->
+          combine_f ( && ) (matches_filter f) (matches_filter g)
+        | FOr (f, g) ->
+          combine_f ( || ) (matches_filter f) (matches_filter g)
+        | FNot f ->
+          begin match matches_filter f with
+            | `Bool b -> `Bool (not b)
+            | `Always ->
+              Logs.info (fun m -> m "matches_filter: not %s resulted in true" (filter_to_string f));
+              `Bool true
+          end
+        | FDefined f ->
+          Logs.info (fun m -> m "matches_filter: defined %s" (filter_to_string f));
+          `Bool true
+        | FUndef f ->
+          Logs.info (fun m -> m "matches_filter: undefined %s" (filter_to_string f));
+          `Bool true
+      in
+      let rec matches_condition = function
+        | OpamTypes.Empty ->
+          Logs.debug (fun m -> m "matches_condition: empty");
+          `Bool true
+        | Atom (OpamTypes.Filter f) ->
+          Logs.debug (fun m -> m "matches_condition: filter %s" (filter_to_string f));
+          matches_filter f
+        | Atom (Constraint (relop, f)) ->
+          Logs.debug (fun m -> m "matches_condition: constraint %s %s"
+                        (string_of_relop relop)
+                        (filter_to_string f));
+          let r =
+            let v =
+              match f with
+              | OpamTypes.FString s -> Some s
+              | FIdent (_opt_names, var, _env) when OpamVariable.to_string var = "version" ->
+                Some (OpamPackage.Version.to_string (OpamFile.OPAM.version opam))
+              | _ -> None
+            in
+            match v with
+            | None ->
+              Logs.info (fun m -> m "matches_condition: unexpected filter %s"
+                            (filter_to_string f));
+              true
+            | Some v ->
+              available :=
+                List.filter (fun ver ->
+                    let r = relop_cmp (OpamVersionCompare.compare ver v) relop in
+                    Logs.debug (fun m -> m "%s %s %s = %B" ver (string_of_relop relop) v r);
+                    r)
+                  !available;
+              !available <> []
           in
-          extract_string one
-        | Some x ->
-          Logs.warn (fun m -> m "%s intent failure: expected a list of a single string, got %s"
-                        pkg (OpamPrinter.FullPos.value x));
-          Ok default
+          Logs.debug (fun m -> m "matches_condition result %B" r);
+          `Bool r
+        | Block a ->
+          Logs.debug (fun m -> m "matches_condition: block");
+          matches_condition a
+        | And (a, b) ->
+          Logs.debug (fun m -> m "matches_condition: and");
+          let a = matches_condition a in
+          let b = matches_condition b in
+          Logs.debug (fun m -> m "matches_condition: and - combining %s with %s"
+                         (match a with `Bool true -> "bool true" | `Bool false -> "bool false" | `Always -> "always")
+                         (match b with `Bool true -> "bool true" | `Bool false -> "bool false" | `Always -> "always"));
+          let r = combine_f ( && ) a b in
+          Logs.debug (fun m -> m "matches_condition: and result %s"
+                         (match r with `Bool true -> "bool true" | `Bool false -> "bool false" | `Always -> "always"));
+          r
+        | Or (a, b) ->
+          Logs.debug (fun m -> m "matches_condition: or");
+          let a = matches_condition a in
+          available := all_available;
+          let b = matches_condition b in
+          available := all_available;
+          combine_f ( || ) a b
       in
-      let intent =
-        try Mintent.M.intent_of_string intent with
-        | Failure _ ->
-          Logs.warn (fun m -> m "%s invalid intent: %s, using any" pkg intent);
-          [ Mintent.M.Last max_int ]
+      let r =
+        match matches_condition condition with
+        | `Bool b -> b && !available <> []
+        | `Always -> true
       in
-      (* if none, we output none *)
-      (match intent with
-       | Mintent.M.Last x :: [] when x = max_int ->
-         (* if any, we output all *)
-         let opams = List.filter not_maintained sorted in
-         let keeping = List.map fst opams in
-         let remove = S.elements (S.diff (S.of_list (List.map fst sorted)) (S.of_list keeping)) in
-         (*Logs.app (fun m -> m "%s intent is any" pkg);*)
-         List.iter (fun pkg -> Logs.app (fun m -> m "REMOVING (any) %s" pkg)) remove
-       | Mintent.M.Last 1 :: [] ->
-         (* latest! we go through all ocaml versions and find the latest package *)
-         let keeping = solve_by_ocaml_version contexts pkg in
-         (* let opams = find_latest sorted in
-         let opams = List.filter not_maintained (M.bindings opams) in
-            let keeping = List.map fst opams in *)
-         let remove = S.elements (S.diff (S.of_list (List.map fst sorted)) keeping) in
-         (*Logs.app (fun m -> m "%s intent is latest" pkg);*)
-         List.iter (fun pkg -> Logs.app (fun m -> m "REMOVING (latest) %s" pkg)) remove
-       | Mintent.M.Last 0 :: [] ->
-         (*Logs.app (fun m -> m "%s intent is none, keeping nothing" pkg);*)
-         List.iter (fun pkg -> Logs.app (fun m -> m "REMOVING (none) %s" pkg)) (List.map fst sorted)
-       | _ ->
-         let opams = List.filter not_maintained sorted in
-         let keeping = List.map fst opams in
-         let remove = S.elements (S.diff (S.of_list (List.map fst sorted)) (S.of_list keeping)) in
-         Logs.warn (fun m -> m "%s intent is %s (not handled)" pkg
-                       (Mintent.M.string_of_intent intent));
-         List.iter (fun pkg -> Logs.app (fun m -> m "REMOVING (%s) %s" (Mintent.M.string_of_intent intent) pkg)) remove
-      );
-      Ok ())
-    (Ok ()) pkgs
+      let e =
+        if not r then begin
+          let cond = condition_to_string condition in
+          let pkg_name, pkg_ver =
+            OpamPackage.Name.to_string (OpamFile.OPAM.name opam),
+            OpamPackage.Version.to_string (OpamFile.OPAM.version opam)
+          in
+          let reason =
+            "\"" ^ OpamPackage.Name.to_string name ^ "\"" ^
+            (if cond = "" then "" else " { " ^ cond ^ " }")
+          in
+          (pkg_name, pkg_ver, reason) :: e
+        end else
+          e
+      in
+      r, e
+    | Block x -> deps_good e x
+    | And (a, b) ->
+      let a', ea = deps_good e a in
+      let b', eb = deps_good ea b in
+      a' && b', eb
+    | Or (a, b) ->
+      let a', ea = deps_good e a in
+      let b', eb = deps_good ea b in
+      a' || b', eb
+  in
+  deps_good [] current_deps
+
+let jump () opam_repository pkgs pkg_all remove_file =
+  let ( let* ) = Result.bind in
+  let pkg_dir = Fpath.(v opam_repository / "packages") in
+  let* _ = Bos.OS.Dir.must_exist pkg_dir in
+  let contexts = List.map (context opam_repository) ocaml_versions in
+  let pkgs = eval_pkgs pkg_dir pkgs pkg_all in
+  (* Phase 1: a set of candidates to remove based on maintenance intent *)
+  let* to_remove =
+    match remove_file with
+    | Some filename ->
+      (* Phase 1a: we already computed this step, and take input from a file *)
+      let* lines = Bos.OS.File.read_lines (Fpath.v filename) in
+      let pkgs =
+        List.fold_left (fun acc line ->
+            if String.starts_with ~prefix:"REMOVING" line then
+              let second_space = succ (String.index_from line 9 ' ') in
+              let pkg =
+                String.sub line second_space (String.length line - second_space)
+              in
+              pkg :: acc
+            else
+              acc
+          ) [] lines
+      in
+      Ok pkgs
+    | None ->
+      (* Phase 1 for real: figure opam file, x-maintenance-intent, solve what to retain *)
+      List.fold_left (fun acc pkg ->
+          let* acc = acc in
+          let* sorted = find_opams_latest_first pkg_dir pkg in
+          let intent = decode_intent pkg (snd (List.hd sorted)) in
+          let to_remove = eval_intent contexts pkg sorted intent in
+          Ok (to_remove @ acc))
+        (Ok []) pkgs
+  in
+  (* Phase 2: evaluate which packages would have unsatisfied dependencies *)
+  let all_opams =
+    (OpamPackage.keys
+       (OpamRepositoryState.load_opams_from_dir
+          (OpamRepositoryName.of_string "temporary")
+          (OpamFilename.Dir.of_string opam_repository)))
+  in
+  let all_opams =
+    List.fold_left (fun opams rm ->
+        OpamPackage.Set.remove (OpamPackage.of_string rm) opams)
+      all_opams to_remove
+  in
+  let foreach path () =
+    match should_consider ~excluded:to_remove path with
+    | None -> ()
+    | Some (pkg_version, opam) ->
+      let r, exp = is_installable all_opams opam in
+      if not r then
+        Logs.app (fun m -> m "pkg %s not installable: %a" pkg_version
+                     Fmt.(list ~sep:(any ", ") string)
+                     (List.map (fun (_, _, r) -> r) exp))
+  in
+  let* r =
+    Bos.OS.Dir.fold_contents foreach () pkg_dir
+  in
+  (* Phase 3: figure out what to not delete
+     (run the solver again to figure which exact versions to restore) *)
+  ignore (r);
+  Ok ()
 
 let setup_log style_renderer level =
   Fmt_tty.setup_std_outputs ?style_renderer ();
@@ -256,10 +527,14 @@ let opam_repository =
   let doc = "Opam repository directory to work on (must be a git checkout)" in
   Arg.(value & opt dir "." & info ~doc ["opam-repository"])
 
+let remove_file =
+  let doc = "Skip the maintenance intent check, and provide a file with things to remove" in
+  Arg.(value & opt (some file) None & info ~doc ["remove-file"])
+
 let cmd =
   let info = Cmd.info "maintenance-intent" ~version:"%%VERSION_NUM%%"
   and term =
-    Term.(term_result (const jump $ setup_log $ opam_repository $ pkg $ pkg_all))
+    Term.(term_result (const jump $ setup_log $ opam_repository $ pkg $ pkg_all $ remove_file))
   in
   Cmd.v info term
 
